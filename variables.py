ad277514f4d288dc6758782ddc1e86e2e88cd67c
CPlusPlusNewb <54950790+CPlusPlusNewb@users.noreply.github.com>
2020-12-20 19:57:37 -0800

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,1 +1,19 @@
 #variables.py
+i1 = 33
+i2 = 48
+i3 = 65
+i4 = i3 + 25
+i5 = 97
+
+print('Intergers')
+print(i1,i2,i3,i4,i4)#debug
+c1 = chr(i1)
+c2 = chr(i2)
+c3 = chr(i3)
+c4 = chr(i4)
+c5 = chr(i5)
+print('Characters')
+print('Sum: ', str(i1+i2+i3+i4+i5))
+#concat = c1+c2+c3+c4+c5
+#print('This is now a string: ', concat)
+#print(type(concat))
