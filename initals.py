97deae76852da0fd77d906bb0fcaa8bfd004ba7e
CPlusPlusNewb <54950790+CPlusPlusNewb@users.noreply.github.com>
2020-12-20 19:38:47 -0800

    Create initals.py

---
+++ initals.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+print("PPPP  L     N    N")
+print("P  P  L     NN   N")
+print("P  P  L     N N  N")
+print("PPPP  L     N  N N")
+print("P     L     N   NN")
+print("P     LLLL  N    N")
