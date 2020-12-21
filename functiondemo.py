11c5deaf8f26b52b7510a325697d89bd8f82da1e
CPlusPlusNewb <54950790+CPlusPlusNewb@users.noreply.github.com>
2020-12-20 20:06:59 -0800

    Update functiondemo.py

--- functiondemo.py
+++ functiondemo.py
@@ -1,1 +1,13 @@
+def printstars(n):
+  print(n, ' ', end="")
+  print("* * * * * * * * * *")
+  print("* * * * * * * * * *")
+  print("* * * * * * * * * *")
 
+def main():
+  printstars(1)
+  printstars(2)
+  printstars(3)
+
+if (__name__ == '__main__'):
+  main()
