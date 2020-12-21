84de2da1b25cb15c6f23946c68e665b722ddd3f6
CPlusPlusNewb <54950790+CPlusPlusNewb@users.noreply.github.com>
2020-12-20 19:59:46 -0800

    Update forloop-1.py

--- forloop-1.py
+++ forloop-1.py
@@ -1,1 +1,4 @@
-
+for i in range (0,256):
+  print(i, '', end='')#still remeber when you praised me for showing you the 'end=""' thing so it wouldnt go to the next line, lol
+  if(i % 10 == 0 and i > 0):
+    print()
